72ed7a1cf77c5d74568582d1f5a8e2e5546df482
PKBM Askara <pkbm.askara@gmail.com>
2026-08-17 17:30:42 +0700

    chore: remove test scripts causing build error

--- scripts/test-db.ts
+++
@@ -1,1 +0,0 @@
-import { db } from './src/lib/db'; async function test() { try { const user = await db.user.findUnique({ where: { email: 'syukron.aqiqah@gmail.com' } }); console.log(user); } catch (e) { console.error(e); } } test();  
